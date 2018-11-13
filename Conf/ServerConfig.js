74a935fd577a6beca936688cd18b0e0ec90be139
Tuisku Saarelainen <saarelainen.tuisku@gmail.com>
2018-11-13 14:00:09 +0200

    added procfile for heroku

--- Conf/ServerConfig.js
+++ Conf/ServerConfig.js
@@ -12,9 +12,9 @@ var dbConnection = {
 };
 
 var facebookAPI = {
-    clientID: '',
-    clientSecret: '',
-    callbackURL: '',
+    clientID: 'CLIENT_ID',
+    clientSecret: 'CLIENT_SECRET',
+    callbackURL: 'http://localhost:3000/auth/facebook/callback',
     profileFields: ['id', 'displayName', 'emails']
 }
 
